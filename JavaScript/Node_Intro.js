1549ac1525d9f60c89f9c1fb07497c11728887e8
Selvendran K <selvendranks@gmail.com>
2021-11-13 09:24:13 +0530

    final

--- JavaScript/Node_Intro.js
+++ JavaScript/Node_Intro.js
@@ -1,18 +1,18 @@
-/////////////////////////////// command line argument
+/////////////////////////////// command line argument [node Node_Intro.js drag donkey]
 
 // const args = process.argv.slice(2);
 // for(let arg of args){
 //     console.log(arg);
 // }
 
-///////////////////////////////  creating director uing node (syncronised version)
+///////////////////////////////  creating director uing node (syncronised version) [node Node_Intro.js Untitled]
 
-try{
-const fs = require('fs');
-const folderName = process.argv[2] || 'Untitled';
-fs.mkdirSync(folderName);
-fs.writeFileSync(`${folderName}.html`);
-}
-catch(e){
-    console.log("sorry error",e);
-}
+// try{
+// const fs = require('fs');
+// const folderName = process.argv[2] || 'Untitled';
+// fs.mkdirSync(folderName);
+// fs.writeFileSync(`${folderName}.html`);
+// }
+// catch(e){
+//     console.log("sorry error",e);
+// }

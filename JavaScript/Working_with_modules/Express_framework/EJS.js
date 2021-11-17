8ec2c9db79fbac21094b4159a692e95f86ce3135
Selvendran K <selvendranks@gmail.com>
2021-11-17 15:30:49 +0530

    finaal

--- JavaScript/Working_with_modules/Express_framework/EJS.js
+++ JavaScript/Working_with_modules/Express_framework/EJS.js
@@ -1,10 +1,14 @@
 const express = require('express');
 app = express();
+const path = require('path');
+
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'/views')); //*to excute from any folder
 
 app.listen(3000,()=>{     //*starts up the server to use
     console.log("listeing");
 })
 
-app.get('/',(req,res)=>{
-    res.send("meow meow");
+app.get('/cat',(req,res)=>{
+    res.render("home.ejs");
 })

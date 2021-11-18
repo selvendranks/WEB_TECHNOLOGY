43345da1de08b8a7b45449914d25d32d301947bf
Selvendran K <selvendranks@gmail.com>
2021-11-18 13:54:24 +0530

    final

--- JavaScript/Working_with_modules/Express_framework/EJS.js
+++ JavaScript/Working_with_modules/Express_framework/EJS.js
@@ -1,6 +1,9 @@
 const express = require('express');
 app = express();
+const redditdata = require('./views/data.json');
 const path = require('path');
+
+app.use(express.static("folder_express")) //*include all files in folder
 
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'/views')); //*to excute from any folder
@@ -9,6 +12,23 @@ app.listen(3000,()=>{     //*starts up the server to use
     console.log("listeing");
 })
 
-app.get('/cat',(req,res)=>{
-    res.render("home.ejs");
+app.get('/test',(req,res)=>{
+    const num = Math.floor(Math.random()*100)+1;
+    res.render("home.ejs",{rand : num}); //*value is passed to template or just reffer same variable
+})
+
+app.get('/cats',(req,res)=>{
+    const cats = ["monty","dgry","bakri","karuppan","vellayan"];
+    res.render("loop.ejs",{cats});
+})
+
+app.get('/r/:subreddit',(req,res)=>{
+    
+    const {subreddit} = req.params;
+    const data = redditdata[subreddit];
+    if(data){
+    res.render('reddit.ejs',{data});
+    }
+    else
+       res.send("not found sorry");
 })

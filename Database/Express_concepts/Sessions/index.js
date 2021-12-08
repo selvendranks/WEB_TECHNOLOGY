e8279ce846434344bf1f3fc761cb81a9f6062fac
Selvendran K <selvendranks@gmail.com>
2021-12-08 10:53:20 +0530

    final

--- Database/Express_concepts/Sessions/index.js
+++ Database/Express_concepts/Sessions/index.js
@@ -2,7 +2,7 @@ const express  = require('express');
 const app = express();
 const session = require('express-session');
 
-const sessionOption = {secret: 'secreti',resave:false,}
+const sessionOption = {secret: 'secreti',resave:false,saveUninitialized:false}
 app.use(session(sessionOption));
 
 
@@ -16,6 +16,15 @@ app.get('/viewcount',(req,res)=>{
     res.send(`${req.session.cont}`);
 })
 
+app.get('/register/:username',(req,res)=>{
+    const { username = 'None'} = req.params;
+    req.session.username = username;
+    res.redirect('/greet');
+})
+
+app.get('/greet',(req,res)=>{
+    res.send(`Greetings ${req.session.username}`)
+})
 app.listen(3000,()=>{
     console.log("listening");
 })

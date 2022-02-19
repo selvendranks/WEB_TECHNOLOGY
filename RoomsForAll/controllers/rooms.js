7fc3009e2ee7be72628ccd04f10b4e590ab6e60f
selvendranks <selvendranks@gmail.com>
2022-02-19 12:31:40 +0530

    final

---
+++ RoomsForAll/controllers/rooms.js
@@ -0,0 +1,65 @@
+const Room = require('../models/rooms');
+
+module.exports.index = async(req,res)=>{
+    const rooms = await Room.find({});
+    res.render('rooms/index.ejs',{rooms});
+ }
+
+ module.exports.renderNewForm = (req,res)=>{
+    res.render('rooms/new.ejs');
+}
+
+module.exports.addNewRoom = async (req,res)=>{
+    const room = new Room(req.body.Room);
+    room.author = req.user._id;
+    await room.save()
+    req.flash('sucess','Sucessfully added new room');
+    res.redirect(`/room/${room._id}`);
+}
+
+module.exports.showRoom =async (req,res)=>{
+    
+    const {id} = req.params;
+    const room = await Room.findById(id).populate({
+        path :'reviews',
+        populate:{
+            path:'author'
+        }
+    }).populate('author');
+    console.log(room);
+    if(!room){
+        console.log('nulled');
+        req.flash('error','Cannot find room');
+        res.redirect('/room');
+    }
+    else{
+    res.render('rooms/show.ejs',{room});
+    }
+}
+
+module.exports.updateRoom = async (req,res)=>{
+    const {id} = req.params;
+    const room = await Room.findByIdAndUpdate(id,req.body.Room,{runValidators:false,new:true});
+    req.flash('sucess','Sucessfully updated the room')
+    res.redirect(`/room/${room._id}`);
+}
+
+module.exports.renderNewForm  = async (req,res)=>{
+    const {id} = req.params;
+    const room = await Room.findById(id);
+    if(!room){
+        console.log('nulled');
+        req.flash('error','Cannot find room');
+        res.redirect('/room');
+    }
+    else{
+    res.render('rooms/edit.ejs',{room})
+    }
+}
+
+module.exports.deleteRoom = async (req,res)=>{
+    const {id} = req.params;
+    await Room.findByIdAndDelete(id);
+    req.flash('sucess','Sucessfully deleted the room')
+    res.redirect(`/room`);
+}
--- RoomsForAll/routes/rooms.js
+++ RoomsForAll/routes/rooms.js
@@ -9,68 +9,21 @@ const catchAsync = require('../utils/catchAsync');
 router.use(express.urlencoded({extended : true}));
 router.use(methodOverride('_method'));
 
-  
-router.get('/',catchAsync(async(req,res)=>{
-   const rooms = await Room.find({});
-   res.render('rooms/index.ejs',{rooms});
-}))
-router.post('/',validateRoom,catchAsync(async (req,res)=>{
-      const room = new Room(req.body.Room);
-      room.author = req.user._id;
-      await room.save()
-      req.flash('sucess','Sucessfully added new room');
-      res.redirect(`/room/${room._id}`);
-}))
+const rooms = require('../controllers/rooms');  
 
-router.get('/new',isloggedin,(req,res)=>{
-    res.render('rooms/new.ejs');
-})
+router.get('/',catchAsync(rooms.index));
 
-router.get('/:id',isloggedin, catchAsync(async (req,res)=>{
-    
-    const {id} = req.params;
-    const room = await Room.findById(id).populate({
-        path :'reviews',
-        populate:{
-            path:'author'
-        }
-    }).populate('author');
-    console.log(room);
-    if(!room){
-        console.log('nulled');
-        req.flash('error','Cannot find room');
-        res.redirect('/room');
-    }
-    else{
-    res.render('rooms/show.ejs',{room});
-    }
-}))
+router.post('/',validateRoom,catchAsync(rooms.addNewRoom));
 
-router.put('/:id',validateRoom,isAuthor,catchAsync(async (req,res)=>{
-    const {id} = req.params;
-    const room = await Room.findByIdAndUpdate(id,req.body.Room,{runValidators:false,new:true});
-    req.flash('sucess','Sucessfully updated the room')
-    res.redirect(`/room/${room._id}`);
-}))
-router.get('/:id/edit',isloggedin,isAuthor,catchAsync(async (req,res)=>{
-    const {id} = req.params;
-    const room = await Room.findById(id);
-    if(!room){
-        console.log('nulled');
-        req.flash('error','Cannot find room');
-        res.redirect('/room');
-    }
-    else{
-    res.render('rooms/edit.ejs',{room})
-    }
-}))
+router.get('/new',isloggedin,rooms.renderNewForm);
+
+router.get('/:id',isloggedin, catchAsync(rooms.showRoom));
+
+router.put('/:id',validateRoom,isAuthor,catchAsync(rooms.updateRoom));
+
+router.get('/:id/edit',isloggedin,isAuthor,catchAsync(rooms.renderEditForm));
 
 
-router.delete('/:id/delete',catchAsync(async (req,res)=>{
-    const {id} = req.params;
-    await Room.findByIdAndDelete(id);
-    req.flash('sucess','Sucessfully deleted the room')
-    res.redirect(`/room`);
-}))
+router.delete('/:id/delete',catchAsync(rooms.deleteRoom));
 
 module.exports = router;

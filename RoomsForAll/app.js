eceb43bc45d0bbc81731a37c48fb1e0f5369e3b8
selvendranks <selvendranks@gmail.com>
2022-02-19 17:11:28 +0530

    final

--- RoomsForAll/app.js
+++ RoomsForAll/app.js
@@ -1,3 +1,10 @@
+if(process.env.NODE_ENV !== "production"){
+    require('dotenv').config()
+}
+
+console.log(process.env.SECRET)
+console.log(process.env.API_KEY)
+
 const express = require('express');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
--- RoomsForAll/routes/rooms.js
+++ RoomsForAll/routes/rooms.js
@@ -4,7 +4,8 @@ const methodOverride = require('method-override');
 const {isloggedin,isAuthor,validateRoom} = require('../middleware');
 const catchAsync = require('../utils/catchAsync');
 const multer = require('multer');
-const upload = multer({dest: 'uploads/'});
+const {storage} = require('../cloudinary'); 
+const upload = multer({storage});
 
 router.use(express.urlencoded({extended : true}));
 router.use(methodOverride('_method'));
@@ -14,8 +15,9 @@ const rooms = require('../controllers/rooms');
 router.route('/')
       .get(catchAsync(rooms.index))
       //.post(validateRoom,catchAsync(rooms.addNewRoom));
-      .post(upload.single('image'),(req,res)=>{
-          res.send(req.body,req.file);
+      .post(upload.array('image'),(req,res)=>{
+          console.log(req.body,req.files);
+          res.send("g");
       })
 
 
